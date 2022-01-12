cd7aac427647dcaf928836b51c3eadb71026747b
sosoYim <sohyung.yim@gmail.com>
2022-01-12 13:53:42 +0900

    feat: Insert svg-sprite sheet in index.html
    - Insert at the end of the body

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,27 +1,36 @@
 import React, { ReactElement } from 'react';
 import { App as AppComponent, AppLogo, AppHeader, AppLink } from './App.styled';
 import Logo from '@/assets/logo.svg';
-import { Card } from '@/components';
+import { Card, Icon } from '@/components';
+import { GlobalSvgSprite } from '@/components/GlobalSvgSprite/GlobalSvgSprite';
 
-function App(props): ReactElement {
+interface AppProps {
+  color: string;
+}
+
+function App(props: AppProps): ReactElement {
   return (
-    <AppComponent className="App">
-      <AppHeader bgColor={props.color} className="App-header">
-        <AppLogo src={Logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <AppLink
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </AppHeader>
-      <Card />
-    </AppComponent>
+    <>
+      <AppComponent className="App" style={{ background: 'black' }}>
+        <AppHeader bgColor={props.color} className="App-header">
+          <AppLogo src={Logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <AppLink
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </AppLink>
+        </AppHeader>
+        <Card />
+        <Icon type="favorite" />
+      </AppComponent>
+      <GlobalSvgSprite />
+    </>
   );
 }
 
