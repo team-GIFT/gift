25db77b4bce61894fda34f236bba66c3a5a483d8
sosoYim <sohyung.yim@gmail.com>
2022-01-12 20:30:14 +0900

    feat: Seperate types from component

--- src/components/SvgIcon/SvgIcon.tsx
+++ src/components/SvgIcon/SvgIcon.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
 import sprites from './sprites.svg';
-
-type SvgIconId =
-  | 'favorite'
-  | 'link'
-  | 'more'
-  | 'search'
-  | 'search'
-  | 'sound-off'
-  | 'sound-on'
-  | 'verified';
-
-interface SvgIconProps {
-  id: SvgIconId;
-  width?: number;
-  height?: number;
-  fill?: string;
-}
-
+import { SvgIconProps } from './SvgIcon.type';
 export function SvgIcon({
   id,
   width = 64,
