a766c10ade7c69ef3f8327a867634d573282313c
sosoYim <sohyung.yim@gmail.com>
2022-01-13 23:42:33 +0900

    feat: Implement FormInput - Issue #34

---
+++ src/components/FormInput/FormInput.styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  margin-bottom: 4px;
+`;
+
+export const Input = styled.input`
+  padding: 0.3em 0.6em;
+  font-size: 1rem;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as A11yHidden } from './A11yHidden/A11yHidden';
 export * from './SvgIcon/SvgIcon';
 export * from './Card/Card';
+export * from './FormInput/FormInput';
