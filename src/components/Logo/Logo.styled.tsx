9bffbb0acdbcea02dcaf33203a353a50e4858261
sosoYim <sohyung.yim@gmail.com>
2022-01-14 16:45:11 +0900

    feat: Logo component only imoprt svg
    - This is temparary component which just imoprt svg image

---
+++ src/components/Logo/Logo.styled.tsx
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import GiftLogo from './assets/GiftLogo.svg?react';
+
+export const StyledLogo = styled(GiftLogo)``;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import GiftLogo from './assets/GiftLogo.svg';
+import { StyledLogo } from './Logo.styled';
+
+export function Logo() {
+  return (
+    <div>
+      {/* <img alt="" src={GiftLogo} /> */}
+      <StyledLogo />
+    </div>
+  );
+}
