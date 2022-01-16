c24cbfd8afc06a9d411fcdca54bb1645a7d613a9
barum_park <barumming@gmail.com>
2022-01-16 20:44:13 +0900

    style: Implement loading animation

--- src/components/LoadingSpinner/LoadingSpinner.styled.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.styled.tsx
@@ -1,6 +1,20 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { LoadingSpinnerItemProps } from './LoadingSpinner.types';
 import { color } from '@/styles/theme';
+
+const loading = keyframes`
+  0% {
+    transform: scale(1) translateY(0);
+  }
+  50% {
+    transform: scale(1.5) translateY(-10px);
+
+  }
+  100% {
+    transform: scale(1) translateY(0);
+
+  }
+`;
 
 export const StyledLoadingSpinnerWrap = styled.div`
   display: flex;
@@ -11,7 +25,20 @@ export const StyledLoadingSpinnerItem = styled.div<LoadingSpinnerItemProps>`
   width: ${({ $size }) => $size}px;
   height: ${({ $size }) => $size}px;
   margin: 10px;
+  animation: ${loading} 1s infinite;
   background: ${({ $color }) => {
     return color[$color];
   }};
+  &:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  &:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+  &:nth-child(4) {
+    animation-delay: 0.6s;
+  }
+  &:nth-child(5) {
+    animation-delay: 0.8s;
+  }
 `;
