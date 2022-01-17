f49575ec84e5450bbb66d1b3bfa5b575e54412a8
sosoYim <sohyung.yim@gmail.com>
2022-01-17 15:49:01 +0900

    delete: Delete useless code in main

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,7 @@
 import React, { ReactElement } from 'react';
-import { App as AppComponent, AppLogo, AppHeader, AppLink } from './App.styled';
 import { Logo } from '@/components';
 
-interface AppProps {
-  color: string;
-}
-
-function App(props: AppProps): ReactElement {
+function App(): ReactElement {
   return (
     <div style={{ height: '98vh', display: 'flex', alignItems: 'center' }}>
       <Logo height={100} />
