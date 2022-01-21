d6815affd978f4fdee24ad27136e5640d2973639
jkpark104 <jkrang104@gmail.com>
2022-01-22 07:25:20 +0900

    refactor: Modify Grid.stories.tsx
    1. delete unnecessary codes

--- src/components/Grid/Grid.stories.tsx
+++ src/components/Grid/Grid.stories.tsx
@@ -10,18 +10,3 @@ export default {
 const Template: ComponentStory<typeof Grid> = () => <Grid />;
 
 export const Grid1 = Template.bind({});
-Grid1.args = {
-  title: 'hihi',
-  url: 'https://giphy.com/gifs/studiosoriginals-TmhCoUsBQrzWrcsqAd',
-  preview: {
-    height: 228,
-    mp4: 'https://media2.giphy.com/media/TmhCoUsBQrzWrcsqAd/giphy-preview.mp4?cid=cba2429cg7wsgc2opasvqed5sf3rge2u3o45dpf2d2zmldey&rid=giphy-preview.mp4&ct=g',
-    mp4_size: '38984',
-    width: 304,
-  },
-  user: {
-    image: 'https://media3.giphy.com/avatars/studiosoriginals/YkMcrX3ELQ7C.gif',
-    link: 'https://giphy.com/studiosoriginals/',
-    name: 'park',
-  },
-};
