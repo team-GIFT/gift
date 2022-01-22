1a6e06f4a0b7f42a82591629ba166a05727332f7
sosoYim <sohyung.yim@gmail.com>
2022-01-22 21:33:46 +0900

    feat: Appear SubMenu comp when hover
    - isActive class in GlobalNav component
    - Keep thinking if control isActibe in Header component

--- src/components/GlobalNav/GlobalNav.styled.tsx
+++ src/components/GlobalNav/GlobalNav.styled.tsx
@@ -3,6 +3,17 @@ import { px2rem } from '@/styles';
 
 const globalNavHeight = px2rem(36);
 
+export const StyledNav = styled.nav`
+  .sub-menu.isActive {
+    display: flex !important;
+    position: absolute;
+    /* TODO: 높이 조절시 재사용에 용이하지 않다. */
+    /* Header에서 통제할지 고민 */
+    top: ${px2rem(68)};
+    left: 0;
+  }
+`;
+
 export const StyledGlobalNav = styled.ul`
   background: linear-gradient(
       to right,
@@ -17,6 +28,10 @@ export const StyledGlobalNav = styled.ul`
   display: flex;
   justify-content: space-around;
   min-width: 496px;
+
+  & + .sub-menu {
+    display: none;
+  }
 `;
 
 export const StyledGlobalNavItems = styled.li`
