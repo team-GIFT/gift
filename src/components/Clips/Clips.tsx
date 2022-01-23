e148c473fda282a43f4aa55a40100cc9e4de215b
jkpark104 <jkrang104@gmail.com>
2022-01-24 00:57:38 +0900

    fix: Modify codes apply redux state
    1. add useSelector hook

--- src/components/Clips/Clips.tsx
+++ src/components/Clips/Clips.tsx
@@ -6,32 +6,26 @@ import {
 } from './Clips.styled';
 import { Card } from '@/components';
 
-export function Clips() {
-  const card = {
-    // redux global로 받아올 것
-    title: 'hihi',
-    url: 'https://giphy.com/gifs/lego-baby-yoda-legos-lego-star-wars-holiday-special-l7fdqmHQ1jCg2HzQlx',
-    preview: {
-      height: 238,
-      width: 423,
-      mp4_size: '26021',
-      mp4: 'https://media4.giphy.com/media/zWGsVQuMFQUfaY3lTu/giphy.mp4?cid=cba2429c38f8yhuyvnaicqo7xzpmnptwffvsd7eyizrz88lx&rid=giphy.mp4&ct=v',
-    },
-    user: {
-      image: 'https://media3.giphy.com/channel_assets/lego/OZpuno6GstOM.png',
-      link: 'https://giphy.com/lego/',
-      name: 'park',
-    },
-    containerType: 'clips',
-  };
+import { trendingClipsSelector } from '@/store/featrues/giphy/giphy';
+import { useSelector } from 'react-redux';
+
+export function Clips(): JSX.Element {
+  const { isLoading, cards } = useSelector(trendingClipsSelector);
+
+  const [primaryCard, ...subCards] = cards;
 
   return (
-    <StyledSection className="clips">
-      <StyledPrimaryCard {...card} />
-      <StyledWrapper>
-        <Card {...card} />
-        <Card {...card} />
-      </StyledWrapper>
-    </StyledSection>
+    <>
+      {!isLoading && (
+        <StyledSection className="clips">
+          <StyledPrimaryCard containerType="clips" {...primaryCard} />
+          <StyledWrapper>
+            {subCards.map((card) => (
+              <Card key={card.id} containerType="clips" {...card} />
+            ))}
+          </StyledWrapper>
+        </StyledSection>
+      )}
+    </>
   );
 }
