c2988ed7bfe096bc5b953bbf70a4aead3dbc6f89
sosoYim <sohyung.yim@gmail.com>
2022-01-25 15:05:43 +0900

    style: Delete comments - Close #120

--- src/components/SearchSuggestions/SearchSuggestions.tsx
+++ src/components/SearchSuggestions/SearchSuggestions.tsx
@@ -10,15 +10,14 @@ function Suggestion({ word }: { word: string }) {
 }
 
 export function SearchSuggestions({ keyword }: SearchSuggestionsProps) {
-  // suggestions를 상태로 관리하면 Too many re-renders 에러 발생
-  // const [suggestions, setSuggestions] = useState([]);
   let suggestions: string[] = [];
 
   // debounce 된 키워드가 새로 들어오면 검색을 시작
   const { data, isLoading } = useGetAutoCompleteQuery(keyword);
+
   // 로딩이 끝나면 받아올 data
-  !isLoading && console.log('suggestions', data?.data);
-  // 리스트 랜더링을 위한 배열
+  // !isLoading && console.log('suggestions', data?.data);
+
   // 로딩이 끝나면 필요한 값(연관 검색어)를 suggestions 배열에 담는다
   if (!isLoading)
     suggestions = data?.data.map((element: { name: string }) => element.name);
