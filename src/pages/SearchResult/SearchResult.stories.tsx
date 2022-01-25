c4cf0027ccc9d9fa8ade99f8528d972851372a8e
sosoYim <sohyung.yim@gmail.com>
2022-01-25 12:28:09 +0900

    feat: Basic structure of SearchResult page

---
+++ src/pages/SearchResult/SearchResult.stories.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import { default as SearchResult } from './SearchResult';
+
+// export default {
+//   title: 'pages/SearchResult',
+//   component: SearchResult,
+// } as ComponentMeta<typeof SearchResult>;
+
+// const Template: ComponentStory<typeof SearchResult> = () => <SearchResult />;
+
+// export const SearchResultPage = Template.bind({});
