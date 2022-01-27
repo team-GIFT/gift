eee7d430d97f18a3bd3a01c4d346dcc9756812a2
barum_park <barumming@gmail.com>
2022-01-27 19:21:57 +0900

    feat: Implement PageNotFound page
    - modify taglist logic for search

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { useGetRandomGifQuery } from '@/services';
+import {
+  StyledErrorImageWrap,
+  StyledErrorHeading,
+  StyledErrorLink,
+} from './PageNotFound.styled';
 
 export default function PageNotFound() {
-  return <div>PageNotFound 도착</div>;
+  const { data, isLoading } = useGetRandomGifQuery('404 error');
+
+  console.log(data);
+
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <Helmet>
+            <title>Page Not Found</title>
+          </Helmet>
+          <StyledErrorImageWrap>
+            <img src={data?.images.original.webp} alt="404 error" />
+          </StyledErrorImageWrap>
+          <StyledErrorHeading>
+            Oops! There’s nothing here. <br />
+            Please Back to the Home!
+          </StyledErrorHeading>
+          <p
+            css={`
+              text-align: center;
+              margin-bottom: 30px;
+            `}
+          >
+            <StyledErrorLink to="/">Back to the HOME</StyledErrorLink>
+          </p>
+        </>
+      )}
+    </>
+  );
 }
